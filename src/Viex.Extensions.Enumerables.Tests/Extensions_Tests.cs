6656a83edb040aa7e454e2861a93d545c189820a
viex <carp_97@outlook.com>
2022-09-26 16:09:32 -0500

    Added tests for JoinString methods.

--- src/Viex.Extensions.Enumerables.Tests/Extensions_Tests.cs
+++ src/Viex.Extensions.Enumerables.Tests/Extensions_Tests.cs
@@ -15,4 +15,28 @@ public class Extensions_Tests
         var source = new int[] { 1, 2, 3 };
         Assert.True(source.Empty() == false, "Source must NOT be empty");
     }
+
+    [Theory]
+    [InlineData(new string[] { "This", "Is", "The", "Output" }, "ThisIsTheOutput")]
+    public void JoinStringShouldSucceed(string[] input, string expectedOutput)
+    {
+        var realOutput = input.JoinString();
+        Assert.True(realOutput == expectedOutput, @$"Output should be ""{expectedOutput}""");
+    }
+
+    [Theory]
+    [InlineData(new string[] { "api", "users", "25" }, "api/users/25")]
+    public void JoinStringWithCharSeparatorShouldSucceed(string[] input, string expectedOutput)
+    {
+        var realOutput = input.JoinString('/');
+        Assert.True(realOutput == expectedOutput, @$"Output should be ""{expectedOutput}""");
+    }
+
+    [Theory]
+    [InlineData(new string[] { "api", "users", "25" }, "api/users/25")]
+    public void JoinStringWithStringSeparatorShouldSucceed(string[] input, string expectedOutput)
+    {
+        var realOutput = input.JoinString("/");
+        Assert.True(realOutput == expectedOutput, @$"Output should be ""{expectedOutput}""");
+    }
 }
